f7bed6933051e483e7560a0b6e8ee00e3fd2552d
Mayan Tiwari <mayantiwari647@gmail.com>
2025-05-10 19:04:37 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mayan Tiwari',
+    author_email= 'mayantiwari647@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
